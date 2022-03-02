2f6cbcc3e51b8c5c1bf61c89b00469f5c7d46ca3
Harry Fitzgerald <harry.fitzgerald@truepill.com>
2022-03-02 10:14:35 +0000

    refactor running state

--- src/pomo.js
+++ src/pomo.js
@@ -1,28 +1,37 @@
 export default class Pomo {
   constructor(musicPlayer) {
+    this.isRunning = false;
     this.musicPlayer = musicPlayer;
     this.curTimeout = null;
     this.timeInput = document.getElementById("pomo-time");
     this.pomoBtn = document.getElementById("btn-pomo");
+    this.pomoEndBtn = document.getElementById("btn-pomo-end");
+    this.pomoEndBtn.addEventListener("click", this.clearPomo.bind(this));
     this.pomoBtn.addEventListener("click", this.startPomo.bind(this));
     this.timeRemainingElem = document.getElementById("time-remaining");
   }
 
-  floorDiff(diff) {
-    return diff & 864e5 & 36e6;
+  clearPomo(ev) {
+    ev?.preventDefault();
+    clearTimeout(this.curTimeout);
+    clearInterval(this.updateInterval);
+    this.updateInterval = null;
+    this.curTimeout = null;
+    this.musicPlayer.pauseVideo();
+    this.timeRemainingElem.textContent = "Pomo timer stopped";
+    this.isRunning = false;
   }
 
   startPomo(ev) {
     ev.preventDefault();
-    if (this.curTimeout) {
-      clearTimeout(this.curTimeout);
-      this.curTimeout = null;
+    if (this.isRunning) {
+      this.clearPomo();
     }
     const fullTime = this.getTime();
     if (!fullTime) return;
     const now = new Date();
     const ends = new Date(now.getTime() + fullTime);
-    setInterval(() => {
+    this.updateInterval = setInterval(() => {
       const curTime = new Date();
       const diffInMilliseconds = ends - curTime;
       const diffInMinutes = ~~(((diffInMilliseconds % 864e5) % 36e5) / 6e4);
@@ -30,6 +39,7 @@ export default class Pomo {
       this.timeRemainingElem.textContent = timestamp;
     }, 1e3);
     this.curTimeout = setTimeout(this.endPomo.bind(this), fullTime);
+    this.isRunning = true;
   }
 
   getTime() {
@@ -40,11 +50,10 @@ export default class Pomo {
   }
 
   endPomo() {
-    this.musicPlayer.pauseVideo();
+    this.clearPomo();
     setTimeout(() => {
       const utterance = new SpeechSynthesisUtterance("Pomo timer is up");
       speechSynthesis.speak(utterance);
     }, 100);
-    this.timeRemainingElem.textContent = "Pomo timer complete!";
   }
 }
